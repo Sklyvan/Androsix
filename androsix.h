0d5d3f5d167d37504013e1785a2b4cff8a555d8a
Sklyvan <joangraciataribo@gmail.com>
2022-01-03 13:40:02 +0100

    Centralized all the imports to one single file.

---
+++ androsix.h
@@ -0,0 +1,9 @@
+#ifndef ANDROSIX_ANDROSIX_H
+#define ANDROSIX_ANDROSIX_H
+#define VERSION 1.0
+
+#include "mno/main_operations.h"
+#include "flt/filetransfer_operations.h"
+#include "dbg/debugging_operations.h"
+
+#endif
